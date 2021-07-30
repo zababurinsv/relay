1c93ddbc21ea5c807f907504c9d9cc14313987bc
szababurin <s.zababurin.v@gmail.com>
2021-07-30 23:51:54 +0300

    init

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ let PeerListener = {
   async function createLibp2p(_options) {
     const defaults = {
       addresses: {
-        listen: ['/ip4/0.0.0.0/tcp/0/ws']
+        listen: [`/ip4/${port}/tcp/0/wss`]
         // TODO check "What is next?" section
         // announce: [`/dns4/web3-star.herokuapp.com/tcp/${port}/wss`]
       },
